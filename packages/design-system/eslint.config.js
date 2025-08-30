1dec495df8d85cd8d1bb3acf7645df23215d3708
Thiago Menezes de Oliveira <thiago.mhx@gmail.com>
2025-08-30 04:08:20 -0400

    readd storybook

--- packages/design-system/eslint.config.js
+++ packages/design-system/eslint.config.js
@@ -1,8 +1,8 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+
 import { config as reactConfig } from '@hfsa/eslint-config/react';
 
-export default [
-  ...reactConfig,
-  {
-    ignores: ['dist/**', 'node_modules/**', '**/*.d.ts', 'coverage/**'],
-  },
-];
+export default [...reactConfig, {
+  ignores: ['dist/**', 'node_modules/**', '**/*.d.ts', 'coverage/**'],
+}, ...storybook.configs["flat/recommended"], ...storybook.configs["flat/recommended"]];
